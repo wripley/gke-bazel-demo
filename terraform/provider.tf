b92220eb53bb22b1156f3f18c68e54ae8464063c
Will Ripley <wripley@google.com>
2019-08-23 09:10:29 -0700

    Updated provider versions for google providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,12 +15,12 @@ limitations under the License.
 */
 
 provider "google" {
-  version = "2.10.0"
+  version = "2.13.0"
   project = var.project
 }
 
 provider "google-beta" {
-  version = "2.12.0"
+  version = "2.13.0"
   project = var.project
 }
 
